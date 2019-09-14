41c7ea53abe00596f3d7f4e42a5ac87c9b7cb313
또르 <hansol416@naver.com>
2019-09-14 18:57:35 +0900

    소수 알고리즘 연습

---
+++ STUDY/prime.cpp
@@ -0,0 +1,22 @@
+// 소수판별 알고리즘
+/*
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	bool is_prime = true;
+	scanf_s("%d", &n);
+
+	for (int i = 2; i < n; i++) {
+		if (n% i == 0){
+			is_prime = false;
+		}
+	}
+	if (is_prime == true){
+		printf("%d is prime", n);
+	}
+	else {
+		printf("%d is not prime", n);
+	}
+}
+*/
