926ba1ea219ec648692132dfc6d1fbbcfa4091dd
9992 <hansol416@naver.com>
2019-09-14 19:00:08 +0900

    폴더 위치 이동

---
+++ STUDY/nxnprintstart.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// n x n 개의 별을 출력하는 프로그램
+
+/*
+void printStars(int number) {
+	for (int i = 0; i < number; i++) {
+		printf("*");
+	}
+}
+int main() {
+	int n;
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++) {
+		printStars(n);
+		printf("\n");
+	}
+	return 0;
+}
+*/
---
+++ STUDY/primeFactorization.cpp
@@ -0,0 +1,17 @@
+// 소인수 분해 알고리즘
+
+/*
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf_s("%d", &n);
+	for (int i = 2; n>1; i++) {
+		if (n % i == 0) {
+			printf("%d ",i);
+			n /= i;
+			i--;
+		}
+	}
+}
+*/
