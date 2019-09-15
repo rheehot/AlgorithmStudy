bd70bafa0fe8d595a5ae8e2dddcecaeccd409bec
9992 <hansol416@naver.com>
2019-09-15 23:50:16 +0900

    pointer 이해

---
+++ STUDY/pointer.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+// 포인터란? 
+// 값을 저장하는 것이 아닌, 값의 위치를 저장하는 변수
+
+int main() {
+	int *myPointer;
+	int a;
+	
+	a = 5;
+	myPointer = &a; //a의 주소값을 저장
+	// 결론 a의 위치를 저장하기 위해서 *을 붙임
+	printf("myPointer 값 : %d \n",*myPointer);//myPoiner 값에 저장된 값 출력
+	printf("a : %d \n",a);//a의 값 출력
+}
