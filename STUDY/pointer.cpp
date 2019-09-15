75dd46150fa202c68c7dba32ae6f10ba0aad1ec9
9992 <hansol416@naver.com>
2019-09-15 23:51:19 +0900

    주소값 출력에 대한 내용 추가

--- STUDY/pointer.cpp
+++ STUDY/pointer.cpp
@@ -11,5 +11,6 @@ int main() {
 	myPointer = &a; //a의 주소값을 저장
 	// 결론 a의 위치를 저장하기 위해서 *을 붙임
 	printf("myPointer 값 : %d \n",*myPointer);//myPoiner 값에 저장된 값 출력
+    // 만약 그냥 myPointer를 출력하게 된다면, 해당 값은 myPointer의 위치 값이 나옴
 	printf("a : %d \n",a);//a의 값 출력
 }
